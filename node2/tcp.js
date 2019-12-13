42886ee4746c12064a23dfe0c3dce2cbd6005abf
zhangxr <zxrnhy1993@sina.com>
2019-12-13 15:24:52 +0800

    修改文件目录

--- node2/tcp.js
+++
@@ -1,7 +0,0 @@
-const net = require('net');
-net.createServer((socket) => {
-    console.log('recive a connect');
-    console.log(socket);
-}).listen(8000, () => {
-    console.log('create tcp server');
-})
